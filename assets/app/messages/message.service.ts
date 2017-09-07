25b8d4b6187bfb587759006adec1a3b3253653dd
Ashish Rawat <ashishr350@gmail.com>
2017-09-07 17:42:17 +0530

    adding a service method to fetch the messages

--- assets/app/messages/message.service.ts
+++ assets/app/messages/message.service.ts
@@ -1,5 +1,5 @@
 import { Message } from './message.model';
-import { Injectable } from "@angular/core";
+import { Injectable, EventEmitter } from "@angular/core";
 import { Http, Response, Headers } from "@angular/http";
 
 import 'rxjs/Rx';
@@ -8,6 +8,7 @@ import { Observable } from "rxjs/Observable";
 @Injectable()
 export class MessageService{
     private messages: Message[] = [];
+    messageIsEdit = new EventEmitter<Message>();
 
     constructor(private http: Http ){
 
@@ -20,15 +21,28 @@ export class MessageService{
         return this.http.post("http://localhost:3000/message", body, {headers: headers})
             .map((response: Response) => response.json())
             .catch((error: Response) => Observable.throw(error.json()));
-
-    }
+    } 
 
     getMessages(){
-        return this.messages;
+        return this.http.get('http://localhost:3000/message')
+        .map((response: Response) => {
+            const messages = response.json().obj;
+            let transformedMessages: Message[] = [];
+            for(let msg of messages){
+                transformedMessages.push(new Message(msg.content,"ashish",msg.id,null));
+            }
+            this.messages = transformedMessages;
+            return transformedMessages;
+        })
+        .catch((error: Response) => Observable.throw(error.json()));
+    }
+
+
+    onMessageEdit(){
+
     }
 
     deleteMessage(message:Message){
         this.messages.splice(this.messages.indexOf(message),1);
-
     }
 }
--- routes/messages.js
+++ routes/messages.js
@@ -3,6 +3,25 @@ var router = express.Router();
 
 var Message = require("../models/message");
 
+
+router.get("/",function(req, res, next){
+    Message.find()
+        .exec(function(err, messages){
+            if(err){
+                return res.status(500).json({
+                    title: "Fetching failed due to error",
+                    error: err
+                });
+            }
+            res.status(200).json({
+                message: "Data fetched sucessfully",
+                obj: messages
+            });
+
+        });
+
+});
+
 router.post('/', function (req, res, next) {
     var message = new Message({
         content: req.body.content
